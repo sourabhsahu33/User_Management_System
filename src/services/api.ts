bd917e3ac55ceecf3fde87232c37b1a0cc5c2b6d
learn github <sourabh.sahu10033@gmail.com>
2024-09-21 20:05:09 +0530

    first commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://localhost:5000/api',
+  baseURL: 'http://localhost:5000/api',
   baseURL: 'https://user-management-system-backend-b0ig.onrender.com',
 });
 
